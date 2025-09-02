38e8d85bc59d38c4268d36bc33ca09028c4ebd1d
nicolas-nb <nicolas.santana125@gmail.com>
2025-09-02 09:25:52 -0300

    versao fs e os

---
+++ nodejs/modulos/fs/app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+// Lendo o conteúdo de um arquivo
+fs.readFile('example.txt', 'utf8', (erro, conteudo_do_arquivo) => {
+    if (erro) {
+        console.error('Erro ao ler o arquivo:', erro);
+        return;
+    }
+    console.log('Conteúdo do arquivo:', conteudo_do_arquivo);
+});
+
+console.log('Esta mensagem aparece primerio');
+// A leitura do arquivo é assíncrona, então esta mensagem aparece antes do conteúdo do arquivo
