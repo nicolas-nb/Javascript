dd8c9b19f95fa0e5e1c808c6b72e9376f84e7ce9
Nicolas de Matos Santana Daitx <nicolas.santana1245@gmail.com>
2025-10-14 09:26:03 -0300

    adicao de genero no formulario

---
+++ nodejs/saudacao-mvc/models/saudacaoModel.js
@@ -0,0 +1,20 @@
+module.exports = {
+  gerarMensagemPersonalizada: (nome, idade, genero) => {
+    let faixaEtaria;
+    
+    if (idade <= 3) {
+      faixaEtaria = "Saimon";
+    }
+     else if (idade < 12) {
+      faixaEtaria = "criança";
+    } else if (idade < 18) {
+      faixaEtaria = "adolescente";
+    } else if (idade < 60) {
+      faixaEtaria = "adulto";
+    } else {
+      faixaEtaria = "idoso";
+    }
+
+    return `Olá, ${nome}! Você é um(a) ${faixaEtaria}, seu genero é ${genero}.`;
+  }
+};
